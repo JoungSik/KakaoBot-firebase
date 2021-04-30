f6de1b21de4518b3108bf985720db8a17f1a9911
JoungSik <tjstlr2010@gmail.com>
2021-04-30 17:06:52 +0900

    [Add] 코로나 API deploy 오류 수정

--- functions/index.js
+++ functions/index.js
@@ -5,15 +5,8 @@ const express = require("express");
 admin.initializeApp();
 const app = express();
 
-const hangangRouter = require("../functions/route/hangang");
-const covidRouter = require("../functions/route/covid");
+const covidRouter = require("./route/covid");
 
-app.get("/", (req, res) => {
-  const result = { "name": "JoungSik" };
-  res.status(200).json(result);
-});
-
-app.use("/hangang", hangangRouter);
 app.use("/covid", covidRouter);
 
-exports.v1 = functions.https.onRequest(app);
+module.exports.v1 = functions.https.onRequest(app);
