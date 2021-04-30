b9042f14b57bcc3365d6c65256455cc6d57c35dd
JoungSik <tjstlr2010@gmail.com>
2021-04-30 14:57:28 +0900

    [Add] 키 숨기기

--- functions/index.js
+++ functions/index.js
@@ -16,33 +16,4 @@ app.get("/", (req, res) => {
 app.use("/hangang", hangangRouter);
 app.use("/covid", covidRouter);
 
-app.get("/hello", (req, res) => {
-  const date = new Date();
-  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
-  res.send(`
-    <!doctype html>
-    <head>
-      <title>Time</title>
-      <link rel="stylesheet" href="/style.css">
-      <script src="/script.js"></script>
-    </head>
-    <body>
-      <p>In London, the clock strikes:
-        <span id="bongs">${'BONG '.repeat(hours)}</span></p>
-      <button onClick="refresh(this)">Refresh</button>
-    </body>
-  </html>`);
-});
-
 exports.v1 = functions.https.onRequest(app);
-
-
-// const functions = require("firebase-functions");
-//
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", { structuredData: true });
-//   response.send("Hello from Firebase!");
-// });
